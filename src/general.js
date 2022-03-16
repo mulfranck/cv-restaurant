1e684db77a81570b793ae5496d1f4a822460fc7d
mulfranck <mulfranck5@gmail.com>
2022-03-16 03:39:43 -0700

    elc updated
    elc updated to add text content into the created element.
    the content is the second argument.

--- src/general.js
+++ src/general.js
@@ -1,14 +1,16 @@
 // General is a collection of functions aided at
 // reducing home lots of redundancy
 
-const elc = (tag, cls) => {
-  // this function is simple to create a dome element.
+const elc = (tag, content = null, cls) => {
+  /* this function is simple to create a dome element.*/
   // give that element a class name cls
 
   let el = document.createElement(tag);
 
   // if cls is given then add a class attribute for the element
   if (cls) el.classList.add(cls);
+
+  if (content != null) el.innerText = content;
 
   return el;
 };
