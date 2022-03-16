1810e69d80c1e7106213f3210de1f6be3d8c6bb1
mulfranck <mulfranck5@gmail.com>
2022-03-16 03:25:58 -0700

    Nav-bar works and load the required page.
    Nav-bar function pageLoader takes the target innerText and
    switch through it passing the conresponding page up to the parent index.js
    with the updater function which removes the removable section and appends the passed section

--- src/Sections/Nav-bar.js
+++ src/Sections/Nav-bar.js
@@ -1,25 +1,43 @@
 import _ from 'lodash';
 import { createList, elc } from '../general';
 import Menu from '../pages/Menu';
-let page = '';
+import Home from '../pages/Home';
 
-const Navbar = (prop) => {
+const Navbar = (updater) => {
   // creating the different element need for the navbar section.
-  const nav_el = elc('nav', 'nav-bar');
-  const header_el = elc('header', 'header');
-  const container_el = elc('div', 'container');
+  const nav_el = elc('nav', null, 'nav-bar');
+  const header_el = elc('header', null, 'header');
+  const container_el = elc('div', null, 'container');
 
-  const h1_el = elc('h1');
-
+  const h1_el = elc('h1', 'Cratley Valley Restaurant');
   const ul_el = createList(
     'ul',
     '<a> Home </a>',
     '<a> Menu </a>',
     '<a> Contact </a>'
   );
+  const pageLoader = (e) => {
+    let value = e.target.innerText;
+    console.log(value);
+    switch (value.toLowerCase()) {
+      case 'contact':
+        console.log('hellow home');
+        break;
+      case 'menu':
+        updater(Menu());
+        break;
+      default:
+        updater(Home());
+        break;
+    }
+  };
+
   // adding contents into the respective el
   h1_el.innerText = 'Cratley Valley Restaurant';
   nav_el.appendChild(ul_el);
+
+  //Adding eventlistener to the nav_el.
+  nav_el.addEventListener('click', pageLoader);
 
   // Adding the elements into the container el for styling properly
   // h1_el first so it be at the top
