e52b49b570a4da1e63d08fc1eef28a416f6c39dd
mulfranck <mulfranck5@gmail.com>
2022-03-16 03:43:52 -0700

    index.js updated
    the reservation section is removed to the home since its only needed there.
    the Navbar is given the updater funtion to added the removableContent.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import Reservation from './Sections/Reservation';
+// import Banner from './Sections/Banner';
+import { elc } from './general';
+import Home from './pages/Home';
 import Footer from './Sections/Footer';
 import Navbar from './Sections/Nav-bar';
 
 const Content = (() => {
+  let removableContent = elc('div');
   const $content = document.getElementById('content');
+  const updateSection = (section) => {
+    if (removableContent.children[0]) removableContent.children[0].remove();
+    removableContent.appendChild(section);
+  };
 
-  $content.appendChild(Navbar());
+  $content.appendChild(Navbar(updateSection));
+  removableContent.appendChild(Home());
 
-  $content.appendChild(Reservation())
+  $content.appendChild(removableContent);
   $content.appendChild(Footer());
   return $content;
 })();
